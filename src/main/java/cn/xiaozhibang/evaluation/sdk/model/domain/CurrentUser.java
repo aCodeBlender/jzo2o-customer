7bd264eec0e27cd6ebf9f4dcf65bca1bd9f01335
acodeblender <2860909590@qq.com>
2026-03-06 21:12:07 +0800

    pom中的xiaoxiaozhibang以及package com.jzo2o.customer.client; 最后目前有问题

---
+++ src/main/java/cn/xiaozhibang/evaluation/sdk/model/domain/CurrentUser.java
@@ -0,0 +1,9 @@
+package cn.xiaozhibang.evaluation.sdk.model.domain;
+
+import lombok.Data;
+@Data
+public class CurrentUser {
+    private String id;
+    private String nickName;
+    private String userAvatar;
+}

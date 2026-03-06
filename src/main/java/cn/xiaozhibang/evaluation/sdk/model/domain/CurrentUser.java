a1f5a1325c03a1c6bf8a82ab598784faeb70a68a
acodeblender <2860909590@qq.com>
2026-03-06 21:17:27 +0800

    初始化工程

--- src/main/java/cn/xiaozhibang/evaluation/sdk/model/domain/CurrentUser.java
+++
@@ -1,9 +0,0 @@
-package cn.xiaozhibang.evaluation.sdk.model.domain;
-
-import lombok.Data;
-@Data
-public class CurrentUser {
-    private String id;
-    private String nickName;
-    private String userAvatar;
-}
